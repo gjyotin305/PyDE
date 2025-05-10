09c3eb90a578514d092de9fb4dfc69a5e996240f
Jyotin Goel <b22ai063@iitj.ac.in>
2025-05-10 17:49:52 +0530

    Fixes, add instructions in README.md
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # PyDE
+
 Your Python IDE in NVIM
+
+## Author
+- gjyotin305
+
+## Features:
+- Auto Completion
+- LSP
+- FuzzyFinder
+- Grep on all files
+- Telescope
+
+## Package Manager Install
+```bash
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+```
+
+## Clone the repository
+```bash
+git clone https://github.com/gjyotin305/PyDE.git ~/.config/nvim
+```
--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 require("gjyotin305")
+-- Line numbers
+vim.opt.number = true         -- Enable absolute line numbers
+vim.opt.relativenumber = false -- Disable relative line numbers
+
+-- Tabs and indentation
+vim.opt.tabstop = 4           -- 1 tab = 4 spaces
+vim.opt.shiftwidth = 4        -- Indent by 4 spaces
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.smartindent = true    -- Smart indentation
+
+-- Optional visual preference
+vim.opt.termguicolors = true  -- Enable true color support
+
